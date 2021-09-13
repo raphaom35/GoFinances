08e578095ac19da4a52eff5d39bcd47d32c8607c
Raphael <rapha35om.rm@gmail.com>
2021-09-13 20:43:37 -0300

    Chapter II aula 3

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text,StyleSheet} from 'react-native';
+
+import {Container,Title} from './styles'
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    )
+
+}
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#cecece',
+        
+    }
+})
