fd09c2d7df61adf84e1edc173fed18d415c5e9e8
Raphael <rapha35om.rm@gmail.com>
2021-09-15 20:24:14 -0300

    Chapter II fim do styled components

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,16 +10,43 @@ import {
     LastTransition,
 } from './styles'
 
-export function HighlightCard(){
+interface Props {
+    type: 'up' | 'down' | 'total';
+    title: string;
+    amount: string;
+    lastTransaction: string;
+  }
+  
+  const icon = {
+    up: 'arrow-up-circle',
+    down: 'arrow-down-circle',
+    total: 'dollar-sign'
+  }
+
+export function HighlightCard({
+    title, 
+    amount, 
+    lastTransaction,
+    type
+    }:Props) {
     return (
-        <Container>
+        <Container type={type}>
             <Header>
-                <Title>Entrada</Title>
-                <Icon name="arrow-up-circle" />
+                <Title type={type}>
+                    {title}
+                </Title>
+                <Icon 
+                    name={icon[type]} 
+                    type={type}
+                />
             </Header>
             <Footer>
-                <Amount>R$ 17.400,00</Amount>
-                <LastTransition>Última entrada dia 13 de abril</LastTransition>
+                <Amount type={type}>
+                    {amount}
+                </Amount>
+                <LastTransition type={type}>
+                    {lastTransaction}
+                </LastTransition>
             </Footer>
         </Container>
     )
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { Transition } from 'react-native-reanimated';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransitionsCards,TransitionsCardProps } from '../../components/TransationCard';
 
 import {
     Container,
@@ -13,7 +16,51 @@ import {
     UserName,
     Icon,
     HighlightCards,
+    Transitions,
+    Title,
+    TransitionList,
 } from './styles'
+
+export interface DataListProps extends TransitionsCardProps{
+    id:string;
+}
+
+const data:DataListProps =[
+    {
+    id:'1',
+    type:'positive',
+    title:"Desenvolvimento de site",
+    amount:"R$ 12.000,00",
+    category:{
+        name:"Vendas",
+        icon:"dollar-sign"
+    },
+    date:"13/04/2020"
+    },
+    {
+        id:'2',
+        type:'negative',
+        title:"Hamburgueria Pizzy",
+        amount:"R$ 59,00",
+        category:{
+            name:"Alimentação",
+            icon:"coffee"
+        },
+        date:"13/04/2020"
+    },
+    {
+        id:'3',
+        type:'negative',
+        title:"Aluguel do apartamento",
+        amount:"R$ 1.200,00",
+        category:{
+            name:"Casa",
+            icon:"shopping-bag"
+        },
+        date:"27/03/2020"
+    },
+
+];
 
 export function Dashboard(){
     return(
@@ -32,10 +79,36 @@ export function Dashboard(){
            
            </Header>
            <HighlightCards> 
-                <HighlightCard/>
-                <HighlightCard/>
-                <HighlightCard/>
+                <HighlightCard 
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    type="up"
+                    lastTransaction="Última entrada dia 13 de abril"
+                    
+                />
+                <HighlightCard 
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                    type="down"
+                />
+                <HighlightCard
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 16 de abril"
+                    type="total"
+                />
             </HighlightCards> 
+            <Transitions>
+                <Title>Listagem</Title>
+                <TransitionList
+                    data={data}
+                    keyExtractor={item=>item.id}
+                    renderItem={({item})=><TransitionsCards data={item} />}
+                />
+                
+
+            </Transitions>
         </Container>
     )
 
